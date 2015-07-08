60e0f850e2c1a10f79a469e6d4a3244856cab94d
vtlmks <vtlmks@gmail.com>
2015-07-08 18:50:59 +0200

    glsl-shaders + function to load

--- EpicBongoGimp/main.cpp
+++ EpicBongoGimp/main.cpp
@@ -3,7 +3,12 @@
 #include <SDL.h>
 #include <glm/glm.hpp>
 
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <stdint.h>
+#include <string>
+#include <vector>
 
 
 /* -   -  - ---=--=-=-==-===-====<{[ TODO ]}>====-===-==-=-=--=--- -  -   -
@@ -26,9 +31,12 @@ Change to fullscreen use SDL_SetWindowFullscreen(SDL_Window *win, uint32 flags);
 flags: SDL_WINDOW_FULLSCREEN, SDL_WINDOW_FULLSCREEN_DESKTOP or 0(window)
 
 */
+GLuint LoadShaders(const char *vertex_file_path,const char *fragment_file_path);
+
 
 int main(int argc, char *argv[]) {
 	std::cout << "Epic Bongo Gimp V0.0.0 - (c)2015" << std::endl;
+
 
 	if(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER)) {
 		std::cout << "Boohoo motherfucker... Culd nut initializ0r sdl2 something something whatever..." << std::endl;
@@ -51,6 +59,10 @@ int main(int argc, char *argv[]) {
 	SDL_Event event;
 	bool running{true};
 
+	GLuint VertexArrayID;
+	gl::GenVertexArrays(1, &VertexArrayID);
+	gl::BindVertexArray(VertexArrayID);
+
 	gl::ClearColor(.1f, .2f, .4f, 1.f);
 
 	gl::Enable(gl::CULL_FACE);
@@ -58,6 +70,8 @@ int main(int argc, char *argv[]) {
 
 	gl::Enable(gl::DEPTH_TEST);
 	gl::DepthFunc(gl::LESS);
+
+	GLuint shaderProgram = LoadShaders("default_vert.glsl", "default_frag.glsl");
 
 	while(running) {
 		while(SDL_PollEvent(&event)) {
@@ -68,7 +82,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 		gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
-
+		gl::UseProgram(shaderProgram);
 
 
 		SDL_GL_SwapWindow(window);
@@ -78,3 +92,65 @@ int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 }
 
+GLuint LoadShaders(const char *vertex_file_path,const char *fragment_file_path) {
+	GLuint VertexShaderID		= gl::CreateShader(gl::VERTEX_SHADER);
+	GLuint FragmentShaderID	= gl::CreateShader(gl::FRAGMENT_SHADER);
+
+	std::string VertexShaderCode{};
+	std::string FragmentShaderCode{};
+
+	std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+	if(VertexShaderStream.is_open()) {
+		std::string Line = "";
+		while(getline(VertexShaderStream, Line))
+		VertexShaderCode += "\n" + Line;
+		VertexShaderStream.close();
+	}
+
+	std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+	if(FragmentShaderStream.is_open()) {
+		std::string Line = "";
+		while(getline(FragmentShaderStream, Line))
+		FragmentShaderCode += "\n" + Line;
+		FragmentShaderStream.close();
+	}
+
+	GLint Result = gl::FALSE_;
+	int InfoLogLength;
+
+	char const *VertexSourcePointer = VertexShaderCode.c_str();
+	gl::ShaderSource(VertexShaderID, 1, &VertexSourcePointer , 0);
+	gl::CompileShader(VertexShaderID);
+
+	gl::GetShaderiv(VertexShaderID, gl::COMPILE_STATUS, &Result);
+	gl::GetShaderiv(VertexShaderID, gl::INFO_LOG_LENGTH, &InfoLogLength);
+	std::vector<char> VertexShaderErrorMessage(InfoLogLength);
+	gl::GetShaderInfoLog(VertexShaderID, InfoLogLength, 0, &VertexShaderErrorMessage[0]);
+	fprintf(stdout, "%s\n", &VertexShaderErrorMessage[0]);
+
+	char const *FragmentSourcePointer = FragmentShaderCode.c_str();
+	gl::ShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , 0);
+	gl::CompileShader(FragmentShaderID);
+
+	gl::GetShaderiv(FragmentShaderID, gl::COMPILE_STATUS, &Result);
+	gl::GetShaderiv(FragmentShaderID, gl::INFO_LOG_LENGTH, &InfoLogLength);
+	std::vector<char> FragmentShaderErrorMessage(InfoLogLength);
+	gl::GetShaderInfoLog(FragmentShaderID, InfoLogLength, 0, &FragmentShaderErrorMessage[0]);
+	fprintf(stdout, "%s\n", &FragmentShaderErrorMessage[0]);
+
+	GLuint ProgramID = gl::CreateProgram();
+	gl::AttachShader(ProgramID, VertexShaderID);
+	gl::AttachShader(ProgramID, FragmentShaderID);
+	gl::LinkProgram(ProgramID);
+
+	gl::GetProgramiv(ProgramID, gl::LINK_STATUS, &Result);
+	gl::GetProgramiv(ProgramID, gl::INFO_LOG_LENGTH, &InfoLogLength);
+	std::vector<char> ProgramErrorMessage(std::max(InfoLogLength, int(1)));
+	gl::GetProgramInfoLog(ProgramID, InfoLogLength, 0, &ProgramErrorMessage[0]);
+	fprintf(stdout, "%s\n", &ProgramErrorMessage[0]);
+
+	gl::DeleteShader(VertexShaderID);
+	gl::DeleteShader(FragmentShaderID);
+ 
+	return ProgramID;
+}
